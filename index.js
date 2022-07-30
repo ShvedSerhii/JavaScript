1be39b07889e231e00b5876e5536ea69471834da
kostennykovAlex <Kostennykov.a@gmail.com>
2022-07-30 21:19:35 +0300

    решение задачи

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // HOME WORK 1
 // написать функцию которая возвращает строку задом наперед
 
-function reversString(string) {
-    return string
-}
 
+function reversString(string) {
+    let newStr = string.reverse()
+    let reverseStr = ''
+    for (let i = 0; i < newStr.length; i++) {
+      const arrStr = newStr[i];
+      reverseStr = arrStr
+    } 
+    return reverseStr
+  }
+  
+  console.log(reversString('hgagjsvjgv asjdvjhavsjdva bajshgdjhvasdb'));
