50e842cc9ac8d6908e33bb7e148304fd5731593d
Your Name <you@example.com>
2022-07-30 21:24:38 +0300

    у

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 // HOME WORK 1
 // написать функцию которая возвращает строку задом наперед
 
-
-//чекаю результат завтра увечері
-
 function reversString(string) {
-<<<<<<< HEAD
-    return string
-}
 
-=======
     let newStr = string.reverse()
     let reverseStr = ''
     for (let i = 0; i < newStr.length; i++) {
@@ -20,4 +13,3 @@ function reversString(string) {
   }
   
   console.log(reversString('hgagjsvjgv asjdvjhavsjdva bajshgdjhvasdb'));
->>>>>>> 1be39b07889e231e00b5876e5536ea69471834da
