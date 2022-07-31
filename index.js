4e4ed4ff245624ecb9a5f1250510367cc2d45825
kostennykovAlex <Kostennykov.a@gmail.com>
2022-07-31 22:07:40 +0300

    Merge New

--- index.js
+++ index.js
@@ -12,4 +12,20 @@ function reversString(string) {
   
   console.log(reversString('12345677890'));
 
-//   
+// функция принимает строку, ищет в строке числа и возвращает сумму этих чисел
+
+
+function findNumberInString(string) {
+  debugger
+  const countNumberStr = parseInt(string)
+  let countNumber = countNumberStr.split(' ') 
+  let resultNumb = 0
+
+  for (let i = 0; i < countNumber.length; i++) {
+    resultNumb += countNumber[i]
+    
+  }
+  return resultNumb
+}
+
+findNumberInString('ahshbhas6 5463 145643 11')
